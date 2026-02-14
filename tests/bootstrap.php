ee57849ab550a471fdd4994a5abc1ff85310b995
Triopsi <daniel@triopsi.com>
2026-02-14 10:33:58 +0000

    chore: include pluggable.php in PHPUnit bootstrap for enhanced testing capabilities

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -38,6 +38,7 @@ if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 // Give access to tests_add_filter() function.
 require_once "{$_tests_dir}/includes/functions.php";
 require_once "{$_tests_dir}/includes/capabilities.php";
+require_once "{$_tests_dir}/includes/pluggable.php";
 
 /**
  * Manually load the plugin being tested.
