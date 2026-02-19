5ff35e78a09d8c071e561a4d6f91e7bd9db89f37
Triopsi <daniel@triopsi.com>
2026-02-19 14:27:26 +0000

    chore: align variable spacing in unit test for consistency

--- tests/Unit/TestExprdawcBaseOrderClass.php
+++ tests/Unit/TestExprdawcBaseOrderClass.php
@@ -17,11 +17,11 @@ class TestExprdawcBaseOrderClass extends TestCase {
 	 * Tests if Exprdawc_Order_Helper::get_adjustment_value returns the correct value for 'fixed'.
 	 */
 	public function test_get_adjustment_value_fixed() {
-		$field   = array(
+		$field  = array(
 			'price_adjustment_type'  => 'fixed',
 			'price_adjustment_value' => '5.5',
 		);
-		$result  = Exprdawc_Order_Helper::get_adjustment_value( $field, 100.0 );
+		$result = Exprdawc_Order_Helper::get_adjustment_value( $field, 100.0 );
 		$this->assertEquals( 5.5, $result );
 	}
 }
