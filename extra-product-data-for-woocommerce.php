9c4e1bfa0fa072ed0194ea8cf540d266e50109d0
Triopsi <daniel@triopsi.com>
2026-03-11 11:44:09 +0100

    fix: correct formatting in plugin header comments

--- extra-product-data-for-woocommerce.php
+++ extra-product-data-for-woocommerce.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: Extra Product Data for WooCommerce
- * Description: Adds customizable input fields per product in WooCommerce, allowing users to enter extra details on the frontend .
+ * Description: Adds customizable input fields per product in WooCommerce, allowing users to enter extra details on the frontend.
  * Version: 3.0.0
  * Author: Triopsi
- * Author URI: https:// triopsi.dev
+ * Author URI: https://triopsi.dev
  * License: GPLv2 or later
- * License URI: https:// www.gnu.org/licenses/gpl-2.0.html
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: extra-product-data-for-woocommerce
  * WC requires at least: 3.9
  * WC tested up to: 9.4
